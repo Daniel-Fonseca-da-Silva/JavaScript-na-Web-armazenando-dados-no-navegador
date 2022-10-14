8c252d33d0e9a8a724a614d0441d11300ed17d25
Daniel Fonseca da Silva <developer-web@programmer.net>
2022-10-14 00:07:11 -0300

    Criando as validações

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
 const form = document.getElementById('novoItem')
+const lista = document.getElementById("lista")
+
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
-    evento.target[0].value
-    evento.target[1].value
-    console.log(evento)
-    console.log(evento.target.elements['nome'].value)
-    console.log(evento.target.elements['quantidade'].value)
+    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
 })
+
+function criaElemento(nome, quantidade) {
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = quantidade
+
+    novoItem.appendChild(numeroItem)
+    novoItem.innerHTML += nome
+
+    lista.appendChild(novoItem)
+}
